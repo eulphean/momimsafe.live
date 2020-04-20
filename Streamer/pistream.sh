75efea03b08c98929122fadb43a2ee87281d9bf4
Amay Kataria <amay.kataria@gmail.com>
2020-04-20 07:32:11 +0100

    ffmpeg restart script raspberry pi

---
+++ Streamer/pistream.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Run ffmpeg. If it crashes, try again. 
+
+cmd="ffmpeg -loglevel info \
+-f v4l2 -framerate 25 -video_size 640x480 -i /dev/video0 \
+-f mpegts -codec:v mpeg1video -s 640x480 -b:v 1000k -bf 0 \
+https://radiant-oasis-49153.herokuapp.com/momimsafe"
+
+until $cmd ; do
+        echo "restarting ffmpeg command..."
+        sleep 2
+done
