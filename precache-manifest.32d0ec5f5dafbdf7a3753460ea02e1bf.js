e17c986cf0655b7bf71401bfd8fe952eb67549b5
Amay Kataria <amkatari@microsoft.com>
2020-04-21 20:56:57 -0500

    Updates

---
+++ precache-manifest.32d0ec5f5dafbdf7a3753460ea02e1bf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c85b9ce36bb30a437a13f08e9bf59ef",
+    "url": "/momimsafe.live/index.html"
+  },
+  {
+    "revision": "bd39bf07acb3443598e2",
+    "url": "/momimsafe.live/static/css/main.d69e3f92.chunk.css"
+  },
+  {
+    "revision": "f008a6a7552bf1a841cc",
+    "url": "/momimsafe.live/static/js/2.7c2b1cbf.chunk.js"
+  },
+  {
+    "revision": "fb4cf92c4d66c0387aa3469e4893fc9a",
+    "url": "/momimsafe.live/static/js/2.7c2b1cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd39bf07acb3443598e2",
+    "url": "/momimsafe.live/static/js/main.d887ebee.chunk.js"
+  },
+  {
+    "revision": "30a3cd92c18065eb7bff",
+    "url": "/momimsafe.live/static/js/runtime-main.86f5035e.js"
+  },
+  {
+    "revision": "0c6cd72756c6ef3bcaf0337ad7036b56",
+    "url": "/momimsafe.live/static/media/bebasneue-webfont.0c6cd727.woff"
+  },
+  {
+    "revision": "8a018e1125b162bff6b5fbd9179bcce6",
+    "url": "/momimsafe.live/static/media/bebasneue-webfont.8a018e11.woff2"
+  },
+  {
+    "revision": "369e87dabfb0b07ef9f813402a24efb2",
+    "url": "/momimsafe.live/static/media/bilbo-regular-webfont.369e87da.woff2"
+  },
+  {
+    "revision": "b3d46d4c29195ce4b4cf5725b3f79b9a",
+    "url": "/momimsafe.live/static/media/bilbo-regular-webfont.b3d46d4c.woff"
+  },
+  {
+    "revision": "184453072f4d631c731fde9092266d17",
+    "url": "/momimsafe.live/static/media/close.18445307.svg"
+  },
+  {
+    "revision": "87b33e2de3819ca5260319ef546738c9",
+    "url": "/momimsafe.live/static/media/opensans-light-webfont.87b33e2d.woff2"
+  },
+  {
+    "revision": "9ad03de81bbf6cd48ffc44cf2aa4d707",
+    "url": "/momimsafe.live/static/media/opensans-light-webfont.9ad03de8.woff"
+  },
+  {
+    "revision": "65a61f9230a85b44df43968f80a522e5",
+    "url": "/momimsafe.live/static/media/opensans-regular-webfont.65a61f92.woff"
+  },
+  {
+    "revision": "d407a58546593548175615d7621183ef",
+    "url": "/momimsafe.live/static/media/opensans-regular-webfont.d407a585.woff2"
+  }
+]);
