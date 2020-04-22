f38ee11c0b3b851269d2907dcb75d49b3dc6d870
Amay Kataria <amkatari@microsoft.com>
2020-04-22 18:35:56 -0500

    Updates

---
+++ precache-manifest.aa02a150776f95a5e203aca7efcd4a14.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfbbd9b2fa410f945a43cdb339648492",
+    "url": "/index.html"
+  },
+  {
+    "revision": "155b93a735c97dc6ef0d",
+    "url": "/static/css/main.06569de8.chunk.css"
+  },
+  {
+    "revision": "2289815310fffb4c2b3a",
+    "url": "/static/js/2.c01ddc8e.chunk.js"
+  },
+  {
+    "revision": "fb4cf92c4d66c0387aa3469e4893fc9a",
+    "url": "/static/js/2.c01ddc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155b93a735c97dc6ef0d",
+    "url": "/static/js/main.2287d195.chunk.js"
+  },
+  {
+    "revision": "65a9e21833378ff9577e",
+    "url": "/static/js/runtime-main.5c074875.js"
+  },
+  {
+    "revision": "0c6cd72756c6ef3bcaf0337ad7036b56",
+    "url": "/static/media/bebasneue-webfont.0c6cd727.woff"
+  },
+  {
+    "revision": "8a018e1125b162bff6b5fbd9179bcce6",
+    "url": "/static/media/bebasneue-webfont.8a018e11.woff2"
+  },
+  {
+    "revision": "369e87dabfb0b07ef9f813402a24efb2",
+    "url": "/static/media/bilbo-regular-webfont.369e87da.woff2"
+  },
+  {
+    "revision": "b3d46d4c29195ce4b4cf5725b3f79b9a",
+    "url": "/static/media/bilbo-regular-webfont.b3d46d4c.woff"
+  },
+  {
+    "revision": "184453072f4d631c731fde9092266d17",
+    "url": "/static/media/close.18445307.svg"
+  },
+  {
+    "revision": "87b33e2de3819ca5260319ef546738c9",
+    "url": "/static/media/opensans-light-webfont.87b33e2d.woff2"
+  },
+  {
+    "revision": "9ad03de81bbf6cd48ffc44cf2aa4d707",
+    "url": "/static/media/opensans-light-webfont.9ad03de8.woff"
+  },
+  {
+    "revision": "65a61f9230a85b44df43968f80a522e5",
+    "url": "/static/media/opensans-regular-webfont.65a61f92.woff"
+  },
+  {
+    "revision": "d407a58546593548175615d7621183ef",
+    "url": "/static/media/opensans-regular-webfont.d407a585.woff2"
+  }
+]);
