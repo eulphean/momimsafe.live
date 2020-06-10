5a1496bff092dfa24fcdbc95a9bcde802deafec7
Amay Kataria <amkatari@microsoft.com>
2020-06-10 01:38:11 -0500

    Small change to see if it fixed iPhone

--- WebClient/src/components/Body.js
+++ WebClient/src/components/Body.js
@@ -226,6 +226,8 @@ const styles={
 
     lowerMouth: {
         position: 'absolute',  
+        top: '0%',
+        bottom: '0%',
         left: '0%',
         right: '0%',
         backgroundColor: color.faceGrey,
