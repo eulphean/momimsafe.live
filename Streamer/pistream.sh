dfb3f04c4527fd8bdff6305e2c4f264449b5dec0
Amay Kataria <amaykataria@Amays-MacBook-Pro.local>
2020-09-24 22:34:22 -0500

    Change down the resolution

--- Streamer/pistream.sh
+++ Streamer/pistream.sh
@@ -2,8 +2,8 @@
 # Run ffmpeg. If it crashes, try again. 
 
 cmd="ffmpeg -loglevel info \
--f v4l2 -framerate 30 -video_size 640x480 -i /dev/video0 \
--f mpegts -codec:v mpeg1video -s 640x480 -b:v 2000k -bf 0 \
+-f v4l2 -framerate 30 -video_size 320x240 -i /dev/video0 \
+-f mpegts -codec:v mpeg1video -s 320x240 -b:v 2000k -bf 0 \
 https://radiant-oasis-49153.herokuapp.com/momimsafe"
 
 until $cmd ; do
